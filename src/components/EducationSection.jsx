1ef96b16664ca13004301ff0fffee5e0f067157d
thisiszana <zanaadibe@gmail.com>
2025-12-26 02:29:10 +0330

    style(EducationSection): update text styles and structure for improved readability

--- src/components/EducationSection.jsx
+++ src/components/EducationSection.jsx
@@ -4,10 +4,10 @@ export default function EducationSection() {
   return (
     <section>
       <Title>Education</Title>
-      <div>
-        <h4 className="font-bold text-gray-900 mb-1">Associate's Degree</h4>
-        <p className="text-gray-600 text-sm mb-1">Electrical Power Engineering</p>
-        <p className="text-gray-600 text-sm mb-1">Shahid Yazdanpanah University of Sanandaj</p>
+      <div className="*:text-gray-600 *:text-xs">
+        <h4 className="font-bold !text-gray-900">Associate's Degree</h4>
+        <p>Electrical Power Engineering</p>
+        <p>Shahid Yazdanpanah University of Sanandaj</p>
       </div>
     </section>
   );
