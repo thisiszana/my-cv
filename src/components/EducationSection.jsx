6f2ec76bfdbe975567d985a3b39891ad60ed8388
thisiszana <zanaadibe@gmail.com>
2025-12-26 11:49:12 +0330

    style(EducationSection): update font weight of education details to bold for improved emphasis

--- src/components/EducationSection.jsx
+++ src/components/EducationSection.jsx
@@ -4,7 +4,7 @@ export default function EducationSection() {
   return (
     <section>
       <Title>Education</Title>
-      <div className="*:text-gray-600 *:text-xs">
+      <div className="*:text-gray-600 *:text-xs *:font-bold">
         <h4 className="font-bold !text-gray-900">Associate's Degree</h4>
         <p>Electrical Power Engineering</p>
         <p>Shahid Yazdanpanah University of Sanandaj</p>
