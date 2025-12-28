916d1528129da865db0b722d94eabf73fe8a2569
thisiszana <zanaadibe@gmail.com>
2025-12-28 12:11:23 +0330

    style(Title): change heading from h3 to h2 for improved semantic structure

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,13 +2,13 @@ import { twMerge } from 'tailwind-merge';
 
 export default function Title({ children, titleClassName }) {
   return (
-    <h3
+    <h2
       className={twMerge(
         'text-base font-black text-gray-900 pb-1 border-b-2 border-black mb-3',
         titleClassName,
       )}
     >
       {children}
-    </h3>
+    </h2>
   );
 }
